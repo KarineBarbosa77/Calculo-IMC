10acc8ec321a650e566fb7f2e9e532ed79739ef2
KarineBarbosa77 <160532475+KarineBarbosa77@users.noreply.github.com>
2024-03-18 19:24:02 +0000

    Calculo imc

---
+++ imc.js
@@ -0,0 +1,17 @@
+const peso = "70"
+const altura = "1.70"
+const imc = peso/(altura*altura)
+
+if(imc > 40){
+  console.log('Obesidade grau III')
+} else if(imc >= 35 && imc <= 39.9){
+  console.log('Obesidade grau II')
+} else if(imc >= 30 && imc <= 34.9){
+  console.log('Obesidade grau I')
+}else if(imc >= 25 && imc <= 29.9){
+  console.log('Sobrepeso')
+}else if(imc >= 18.5 && imc <= 24.9){
+  console.log('Peso normal')
+}else{
+  console.log('Abaixo do peso')
+}
